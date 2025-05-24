9b0459d86049056a092ccd230b29c60a6ae122b6
Summer498@gmail.com <subore02@gmail.com>
2025-05-24 13:44:03 +0900

    return error

--- lib/sendXMLasJSON.ts
+++ lib/sendXMLasJSON.ts
@@ -6,7 +6,7 @@ export const sendXMLasJSON = (res: NextApiResponse<object | undefined>) => (
   data: string
 ) => {
   if (err) {
-    res.status(500).json(undefined)
+    res.status(500).json(err)
     return
   }
   const json = xml_parser.parse(data);
