ed306eeb69ea495199edd9d420ac5321278460c0
Summer498@gmail.com <subore02@gmail.com>
2025-05-24 14:26:33 +0900

    arrow CORS

--- lib/handler-frame.ts
+++ lib/handler-frame.ts
@@ -6,6 +6,9 @@ export const handlerFrame = (mode:string) => (
   req: NextApiRequest,
   res: NextApiResponse<object | undefined>
 ) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.setHeader("Content-Type", "application/json; charset=utf-8");
 
   const { tune } = req.query;
