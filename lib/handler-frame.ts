002f48a6d72a39f5d57d4de1007b18c492446e6c
Summer498@gmail.com <subore02@gmail.com>
2025-05-24 14:04:09 +0900

    bugfix

--- lib/handler-frame.ts
+++ lib/handler-frame.ts
@@ -10,7 +10,7 @@ export const handlerFrame = (mode:string) => (
 
   const { tune } = req.query;
   readFile(
-    `/example/${tune}/${mode}-${tune}.xml`,
+    `example/${tune}/${mode}-${tune}.xml`,
     "utf8",
     sendXMLasJSON(res)
   )
